e9c151890f63d3c0f050b960883f54b442c4c3ed
cloudsystems22 <cloudsystems22@gmail.com>
2020-11-03 20:51:37 -0300

    Controller cliente

---
+++ src/controllers/clienteController.js
@@ -0,0 +1,8 @@
+const clienteController = {
+    async index(req, res) {},
+    async created(req, res) {},
+    async edit(req,res) {},
+    async delete(req,res) {},
+}
+
+module.exports = clienteController
