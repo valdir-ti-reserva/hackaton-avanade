b99c4329de3dff77cdfbf82b10c7c960e274e9ff
cloudsystems22 <cloudsystems22@gmail.com>
2020-11-03 21:32:51 -0300

    CRUD Clientes

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -1,10 +1,50 @@
+const { Cliente } = require('../models/index');
 const clienteController = {
     async index(req, res) {
-        return res.status(200).json("Rota de clientes")
+        const clientes = await Cliente.findAll();
+
+        return res.status(200).json("Rota de clientes!", clientes)
     },
-    async created(req, res) {},
-    async updated(req,res) {},
-    async delete(req,res) {},
+    async details(req, res){
+        const { id } = req.body
+        const cliente = await Cliente.findOne({ where:{ id }})
+
+        return res.status(200).json(cliente);
+    },
+    async created(req, res) {
+        const { name, email, telefone } = req.body;
+        const now = new Date()
+        try{
+            const newCliente = await Cliente.create({name, email, telefone, createdAt:now, updatedAt: now});
+
+            return res.status(201).json(newCliente);
+
+        } catch (err){
+            console.log(err)
+            res.status(401).send({Erro: "Erro ao inserir cliente " + err})
+        }
+    },
+    async updated(req,res) {
+        const { id, name, email, telefone } = req.body;
+        const now = new Date()
+        try{
+            const atualCliente = await Cliente.update({name, email, telefone, createdAt:now, updatedAt: now}, { where:{ id }});
+
+            return res.status(201).json(atualCliente);
+
+        } catch (err){
+            console.log(err)
+            res.status(401).send({Erro: "Erro ao atualizar cliente " + err})
+        }
+    },
+    async delete(req,res) {
+        const { id } = req.body
+        await Cliente.destroy({
+            where:{ id }
+        })
+
+        return res.status(200).json("Deletado com sucesso!");
+    },
 }
 
 module.exports = clienteController
