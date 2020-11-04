2baec4f012e3ea95372f632df6e6d352c7a5cf73
nelson huaman mendez <nelson_trq@hotmail.com>
2020-11-03 19:44:25 -0500

    carroController

--- src/controllers/carroController.js
+++ src/controllers/carroController.js
@@ -1,10 +1,48 @@
+const { Carro } = require('../models/Carro');
 const carroController = {
     async index(req, res) {
-        return res.status(200).json("")
+        const carros = await Carro.findAll();
+
+        return res.status(200).json("Rota de carros", carros)
     },
-    async created(req, res) {},
-    async updated(req,res) {},
-    async delete(req,res) {},
+    async details(req, res){
+        const { id } = req.body
+        const carro = await Carro.findOne({ where:{ id }})
+
+        return res.status(200).json(carro);
+    },
+    async created(req, res) {
+        const { placa, modelo } = req.body;
+        try{
+            const novoCarro = await Carro.create({placa, modelo});
+
+            return res.status(201).json(novoCarro);
+
+        } catch (err){
+            console.log(err)
+            res.status(401).send({Erro: "Erro ao inserir carro " + err})
+        }
+    },
+    async updated(req,res) {
+        const { id, placa, modelo } = req.body;
+        try{
+            const atualCarro = await Carro.update({placa, modelo}, { where:{ id }});
+
+            return res.status(201).json(atualCarro);
+
+        } catch (err){
+            console.log(err)
+            res.status(401).send({Erro: "Erro ao atualizar carro " + err})
+        }
+    },
+    async delete(req,res) {
+        const { id } = req.body
+        await Carro.destroy({
+            where:{ id }
+        })
+
+        return res.status(200).json("Carro deletado com sucesso!");
+    },
 }
 
 module.exports = carroController
