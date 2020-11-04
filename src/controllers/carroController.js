0555273f386e1a05efd4dd85e1eb0590f1b811b1
nelson huaman mendez <nelson_trq@hotmail.com>
2020-11-03 19:36:31 -0500

    carroController

---
+++ src/controllers/carroController.js
@@ -0,0 +1,10 @@
+const carroController = {
+    async index(req, res) {
+        return res.status(200).json("")
+    },
+    async created(req, res) {},
+    async updated(req,res) {},
+    async delete(req,res) {},
+}
+
+module.exports = carroController
