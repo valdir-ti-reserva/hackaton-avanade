0e438d77e93c606841a606629b7fb4a6113d3dd8
Welington Serafim <serwelington@gmail.com>
2020-11-03 21:56:14 -0300

    Adicionas SEED, CONTROLLER motorista

---
+++ src/controllers/motoristaController.js
@@ -0,0 +1,57 @@
+const { Motorista } = require("../models/index");
+const motoristaController = {
+  async index(req, res) {
+    const clientes = await Cliente.findAll();
+
+    return res.status(200).json("Rota de clientes!", clientes);
+  },
+  async details(req, res) {
+    const { id } = req.body;
+    const cliente = await Cliente.findOne({ where: { id } });
+
+    return res.status(200).json(cliente);
+  },
+  async created(req, res) {
+    const { name, email, telefone } = req.body;
+    const now = new Date();
+    try {
+      const newCliente = await Cliente.create({
+        name,
+        email,
+        telefone,
+        createdAt: now,
+        updatedAt: now,
+      });
+
+      return res.status(201).json(newCliente);
+    } catch (err) {
+      console.log(err);
+      res.status(401).send({ Erro: "Erro ao inserir cliente " + err });
+    }
+  },
+  async updated(req, res) {
+    const { id, name, email, telefone } = req.body;
+    const now = new Date();
+    try {
+      const atualCliente = await Cliente.update(
+        { name, email, telefone, createdAt: now, updatedAt: now },
+        { where: { id } }
+      );
+
+      return res.status(201).json(atualCliente);
+    } catch (err) {
+      console.log(err);
+      res.status(401).send({ Erro: "Erro ao atualizar cliente " + err });
+    }
+  },
+  async delete(req, res) {
+    const { id } = req.body;
+    await Cliente.destroy({
+      where: { id },
+    });
+
+    return res.status(200).json("Deletado com sucesso!");
+  },
+};
+
+module.exports = motoristaController;
