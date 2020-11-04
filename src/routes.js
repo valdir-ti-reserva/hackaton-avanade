d7b6d5fddf28b59ce90facf9ec76cd792e216f16
nelson huaman mendez <nelson_trq@hotmail.com>
2020-11-03 19:48:48 -0500

    update Routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const { Router } = require('express');
 const indexController = require("./controllers/homeController");
 const clienteController = require("./controllers/clienteController");
 const corridaController = require("./controllers/corridaController");
+const carroController = require("./controllers/carroController");
+
 
 const routes = Router();
 
@@ -19,5 +21,11 @@ routes.get("/corrida", corridaController.index);
 routes.post("/corrida", corridaController.created);
 routes.put("/corrida", corridaController.updated);
 routes.delete("/corrida/:id", corridaController.delete);
+
+//Sessão de carros
+routes.get("/carro", carroController.index);
+routes.post("/carro", carroController.created);
+routes.put("/carro", carroController.updated);
+routes.delete("/carro/:id", carroController.delete);
 
 module.exports = routes;
